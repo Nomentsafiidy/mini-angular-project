fa69359a67dc6e577fccfe021d18591bb73e24bd
Nomentsafidy <mnomentsafidy@gmail.com>
2021-10-01 17:54:22 +0300

    build add fabric js

--- fabric-js/src/app/pages/home/home.page.ts
+++ fabric-js/src/app/pages/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { fabric } from 'fabric';
 @Component({
     selector: 'app-home',
     templateUrl: './home.page.html',
@@ -8,5 +8,44 @@ import { Component, OnInit } from '@angular/core';
 export class HomePage implements OnInit {
     constructor() {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.fabricDrawRect();
+    }
+
+    drawRect() {
+        console.log('drawRect work fine !!');
+        // reference canvas element (with id="c")
+        var canvasEl = document.getElementById('work-space') as HTMLCanvasElement;
+
+        // get 2d context to draw on (the "bitmap" mentioned earlier)
+        var ctx = canvasEl?.getContext('2d');
+
+        if (ctx) {
+            ctx.fillStyle = '#e5e5e5';
+            // set fill color of context
+            ctx.fillStyle = 'red';
+
+            // create rectangle at a 100,100 point, with 20x20 dimensions
+            ctx?.fillRect(100, 100, 20, 20);
+        }
+    }
+
+    fabricDrawRect() {
+        console.log('fabric function work!!');
+        // create a wrapper around native canvas element (with id="c")
+        var canvas = new fabric.Canvas('work-space');
+
+        // create a rectangle object
+        var rect = new fabric.Rect({
+            left: 100,
+            top: 100,
+            fill: 'red',
+            width: 20,
+            height: 20,
+        });
+
+        // "add" rectangle onto canvas
+        canvas.add(rect);
+        rect.set('selectable', true);
+    }
 }
